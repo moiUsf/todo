a75e27bc7f5320be34e312c08b4d887545781270
Youssef El Ouatiq <youssefElOuatiq@Youssefs-MacBook-Pro.local>
2016-10-09 21:48:33 +0200

    Aufgaben struktur erstellt

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'  #wenn es nicht klappt, die view new wird nochmal mit den eingegebenen Daten geladen, noch mal versuchen
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description)
   end
 end

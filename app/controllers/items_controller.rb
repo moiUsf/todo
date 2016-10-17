906f3cbdab441c96a910b9a61a46d5eb9d9b07b6
Youssef El Ouatiq <youssefElOuatiq@Youssefs-MacBook-Pro.local>
2016-10-17 22:36:59 +0200

    edit + update ready

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,5 +20,24 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :description)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to items_path(@item)
+
+    else
+      render 'edit'
+    end
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end

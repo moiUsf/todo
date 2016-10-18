1fe2f66627d78f285aa3449ef213d3b6a6a61080
Youssef El Ouatiq <youssefElOuatiq@Youssefs-MacBook-Pro.local>
2016-10-18 22:48:30 +0200

    app ready, time problem resolved

--- db/migrate/20161018203120_create_items.rb
+++ db/migrate/20161018203120_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.0]
     create_table :items do |t|
       t.string :title
       t.text :description
+      t.datetime :completed_at
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018115209) do
+ActiveRecord::Schema.define(version: 20161018203120) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "completed_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.datetime "completed_at"
   end
 
 end

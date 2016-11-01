46c61f7adf0bf09560d145a1a4d107ee465e19f3
Youssef El Ouatiq <youssef.elouatiq@gmail.com>
2016-11-01 12:47:01 +0100

    Association User Item done

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
 	def completed?
 		!completed_at.blank?
 	end
+
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101101541) do
+ActiveRecord::Schema.define(version: 20161101111701) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161101101541) do
     t.datetime "completed_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
